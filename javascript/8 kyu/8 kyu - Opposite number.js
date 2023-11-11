d340173fcb41b9aee2a9b7be80b190776dd97910
evgnixx <149940557+evgnixx@users.noreply.github.com>
2023-11-11 21:51:44 +0500

    feat: add solution "Opposite number" kata
    Add solution "Opposite number" kata.

---
+++ javascript/8 kyu/8 kyu - Opposite number.js
@@ -0,0 +1,13 @@
+//8 kyu
+//Opposite number
+
+//Link:
+//https://www.codewars.com/kata/opposite-number
+
+//Description:
+//Very simple, given an integer or a floating-point number, find its opposite.
+
+//Solution:
+function opposite(number) {
+  return (number * (-1));
+}
