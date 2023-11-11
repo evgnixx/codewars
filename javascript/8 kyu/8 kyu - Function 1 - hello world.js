3693c4896033b0e64c6521a29ccc8ffa486ba36d
evgnixx <149940557+evgnixx@users.noreply.github.com>
2023-11-11 17:39:20 +0500

    feat: add solution "Function 1 - hello world" kata
    Add solution "Function 1 - hello world" kata.

---
+++ javascript/8 kyu/8 kyu - Function 1 - hello world.js
@@ -0,0 +1,14 @@
+//8 kyu
+//Function 1 - hello world
+
+//Link:
+//https://www.codewars.com/kata/function-1-hello-world
+
+//Description:
+//Make a simple function called 'greet' that returns the most-famous "hello world!".
+
+//Solution:
+function greet() {
+  const greetMessage = 'hello world!';
+  return greetMessage;
+}
