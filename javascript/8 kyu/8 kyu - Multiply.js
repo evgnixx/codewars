f3d8beb48f187b91a94f36ecc01ad228e3485188
evgnixx <149940557+evgnixx@users.noreply.github.com>
2023-11-11 13:35:09 +0500

    feat: add solution "Multiply" kata
    Add solution "Multiply" kata.

---
+++ javascript/8 kyu/8 kyu - Multiply.js
@@ -0,0 +1,13 @@
+//8 kyu
+//Multiply
+
+//Link:
+//https://www.codewars.com/kata/multiply
+
+//Description:
+//This code does not execute properly. Try to figure out why.
+
+//Solution:
+function multiply(a, b) {
+  return a * b;
+}
